0eeb68065ecaeacd2f98d31157a046ace8bc9978
takafumi-s <takafumi.s.dev@gmail.com>
2016-01-03 18:36:23 +0900

    fix messeage

--- minimum/provision/function/resultMsg.sh
+++ minimum/provision/function/resultMsg.sh
@@ -7,7 +7,7 @@ function resultMsg() {
 
   script_name=`basename $script .sh`
 
-  [ $status -eq $STATUS_OK ]      && echo "${script}.sh) Execute complete."
-  [ $status -eq $STATUS_ERROR ]   && echo "${script}.sh) Execute error. Show log : ${LOG_DIR}/error.${script_name}.log"
-  [ $status -eq $STATUS_ALREADY ] && echo "${script}.sh) Already done. Skip..."
+  [ $status -eq $STATUS_OK ]      && echo "${script}) Execute complete."
+  [ $status -eq $STATUS_ERROR ]   && echo "${script}) Execute error. Show log : ${LOG_DIR}/error.${script_name}.log"
+  [ $status -eq $STATUS_ALREADY ] && echo "${script}) Already done. Skip..."
 }
