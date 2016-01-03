ec53c87b8142726d47a4257e591fed1a1a0baeac
takafumi-s <takafumi.s.dev@gmail.com>
2016-01-03 18:38:40 +0900

    fix git path

--- minimum/provision/102_tmux.sh
+++ minimum/provision/102_tmux.sh
@@ -6,5 +6,5 @@ sudo yum install -y tmux
 #
 # .tmux.conf
 #
-cp ${GIT_DIR}/document/tmux/.tmux.conf ~/
-sudo cp ${GIT_DIR}/document/tmux/.tmux.conf /root/
+cp ${GIT_DIR}/tmux/.tmux.conf ~/
+sudo cp ${GIT_DIR}/tmux/.tmux.conf /root/
