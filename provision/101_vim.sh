d0b1a7b741807f1efda3be9dc7ad924541b84b99
takafumi-s <takafumi.s.dev@gmail.com>
2016-01-04 18:31:53 +0900

    fixed

--- provision/101_vim.sh
+++ provision/101_vim.sh
@@ -22,7 +22,24 @@ sudo make
 sudo make install
 
 #
+# setting
+#
+cp -a ${GITDIR}/document/vim/.vimrc ~/.vimrc
+
+mkdir -p ~/.vim/colors
+wget "http://www.vim.org/scripts/download_script.php?src_id=6657" -O ~/.vim/colors/wombat.vim
+wget "https://raw.githubusercontent.com/w0ng/vim-hybrid/master/colors/hybrid-light.vim" -O hybrid-light.vim
+wget "https://raw.githubusercontent.com/w0ng/vim-hybrid/master/colors/hybrid.vim" -O hybrid.vim
+
+#
 # neocomplete
 #
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+vim +":NeoBundleInstall" +:q
+
+#
+# root
+#
+sudo cp ~/.vim   /root/
+sudo cp ~/.vimrc /root/
