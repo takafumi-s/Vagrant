1c6000e49f9b70a2375404b295846c7215d750a5
takafumi-s <takafumi.s.dev@gmail.com>
2016-01-13 01:07:03 +0900

    add .gitconfig setting

--- provision/005_git.sh
+++ provision/005_git.sh
@@ -6,11 +6,19 @@ sudo yum install -y git git-daemon git-all
 #
 # config
 #
-git config --global push.default simple
-git config --global user.name $GIT_USER
-git config --global user.email $GIT_EMAIL
+echo ${GIT_USER}
+ls -ld ~
+sudo git config --global push.default simple
+sudo git config --global user.name "${GIT_USER}"
+sudo git config --global user.email "${GIT_EMAIL}"
 
 #
 # clone
 #
 git clone https://github.com/takafumi-s/document.git ${GIT_DIR}
+
+#
+# copy
+#
+sudo cp /root/.gitconfig ${HOME_DIR}/.gitconfig
+chown vagrant:vagrant ${HOME_DIR}/.gitconfig
