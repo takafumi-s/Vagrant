11ab0b7f8dbd87b47780b36b96532d40b5e96a49
takafumi-s <takafumi.s.dev@gmail.com>
2016-05-24 23:13:23 +0900

    version up java

--- provision/301_java_1.8.0.sh
+++ provision/301_java_1.8.0.sh
@@ -1,9 +1,9 @@
 #
 # rpm
-# version 8 Update 66
+# version 8 Update91 
 #
-RPM_URL='http://javadl.sun.com/webapps/download/AutoDL?BundleId=111740'
-RPM_FILE='jre-8u66-linux-x64x.rpm'
+RPM_URL='http://download.oracle.com/otn-pub/java/jdk/8u91-b14/jdk-8u91-linux-x64.rpm'
+RPM_FILE='jre-8u91-linux-x64x.rpm'
 
 wget $RPM_URL -O "${RPM_DIR}/${RPM_FILE}"
 
